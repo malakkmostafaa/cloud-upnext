49611396bb1939d04bb84b4fefb6513f9afa8e2c
Saraahmed05 <sara.ahmed.abdelwahid@gmail.com>
2026-05-16 01:19:22 +0300

    Integrate Cognito authentication and role-based access

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 
 import usersRoutes from "./routes/users.routes.js";
 import teamsRoutes from "./routes/teams.routes.js";
 
-dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,5 @@
+import { Amplify } from "aws-amplify";
+import { cognitoConfig } from "./config/cognito";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -5,6 +7,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import React from "react";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: cognitoConfig.userPoolId,
+      userPoolClientId: cognitoConfig.userPoolClientId,
+      region: cognitoConfig.region,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
