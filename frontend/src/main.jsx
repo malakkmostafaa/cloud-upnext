0da9d1f9bd8575154831fb76b95e609025aedc69
mohameddgaafar <mohamed.gafar@student.giu-uni.de>
2026-05-18 19:19:40 +0300

    test

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,16 @@
 import { Amplify } from "aws-amplify";
 import { cognitoConfig } from "./config/cognito";
-import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App.jsx";
+
 import { AuthProvider } from "./context/AuthContext";
+
 import React from "react";
+
 
 Amplify.configure({
   Auth: {
@@ -18,7 +22,10 @@ Amplify.configure({
   },
 });
 
-createRoot(document.getElementById("root")).render(
+
+createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
